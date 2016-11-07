4a02429af13ad7f0bbf3d707c917354a7c8ccfee
Sam Whited <sam@samwhited.com>
2016-11-06 22:10:55 -0600

    Test remove/map

--- transformer_test.go
+++ transformer_test.go
@@ -5,7 +5,109 @@
 package xmlstream
 
 import (
+	"bytes"
 	"encoding/xml"
+	"fmt"
+	"io"
+	"strings"
+	"testing"
 )
 
 var _ Tokenizer = (*xml.Decoder)(nil)
+
+type tokenizerTest struct {
+	Transform Transformer
+	Input     string
+	Output    string
+	Err       bool
+}
+
+func runTests(t *testing.T, tcs []tokenizerTest) {
+	for i, tc := range tcs {
+		t.Run(fmt.Sprintf("%d", i), func(t *testing.T) {
+			d := tc.Transform(xml.NewDecoder(strings.NewReader(tc.Input)))
+			buf := new(bytes.Buffer)
+			e := xml.NewEncoder(buf)
+			var tok xml.Token
+			var err error
+		decodeloop:
+			for err == nil {
+				tok, err = d.Token()
+				switch {
+				case err != nil && err != io.EOF && !tc.Err:
+					t.Fatalf("Unexpected error decoding token: %s", err)
+				case err == io.EOF:
+					break decodeloop
+				}
+				err = e.EncodeToken(tok)
+			}
+			e.Flush()
+			switch {
+			case err != nil && err != io.EOF && tc.Err:
+			case (err == io.EOF || err == nil) && tc.Err:
+				t.Fatal("Expected error, but did not get one")
+			case buf.String() != tc.Output:
+				t.Fatalf("got=`%s`, want=`%s`", buf.String(), tc.Output)
+			}
+
+		})
+	}
+}
+
+func TestRemove(t *testing.T) {
+	runTests(t, []tokenizerTest{
+		{
+			Transform: Remove(func(t xml.Token) bool { return true }),
+			Input:     `<quote>Foolery, sir, does walk about the orb like the sun,<br/>it shines every where.</quote>`,
+			Output:    ``,
+			Err:       false,
+		},
+		{
+			Transform: Remove(func(t xml.Token) bool {
+				if _, ok := t.(xml.CharData); ok {
+					return false
+				}
+				return true
+			}),
+			Input:  `<quote>Now Jove, in his next commodity of hair, send thee a beard!</quote>`,
+			Output: `Now Jove, in his next commodity of hair, send thee a beard!`,
+			Err:    false,
+		},
+		{
+			Transform: Remove(func(t xml.Token) bool {
+				if _, ok := t.(xml.StartElement); ok {
+					return true
+				}
+				return false
+			}),
+			Input:  `<quote>Now Jove, in his next commodity of hair, send thee a beard!</quote>`,
+			Output: ``,
+			Err:    true,
+		},
+	})
+}
+
+func TestMap(t *testing.T) {
+	runTests(t, []tokenizerTest{
+		{
+			Transform: Map(func(t xml.Token) xml.Token {
+				switch tok := t.(type) {
+				case xml.StartElement:
+					if tok.Name.Local == "quote" {
+						tok.Name.Local = "blocking"
+						return tok
+					}
+				case xml.EndElement:
+					if tok.Name.Local == "quote" {
+						tok.Name.Local = "blocking"
+						return tok
+					}
+				}
+				return t
+			}),
+			Input:  `<quote>[Re-enter Clown with a letter, and FABIAN]</quote>`,
+			Output: `<blocking>[Re-enter Clown with a letter, and FABIAN]</blocking>`,
+			Err:    false,
+		},
+	})
+}
