dc3f7cc61615b4dd629477675ea18665d3cf0e3a
Sam Whited <sam@samwhited.com>
2016-11-06 18:07:56 -0600

    Add some simple Transformer constructors

--- transformer.go
+++ transformer.go
@@ -19,3 +19,64 @@ type Tokenizer interface {
 // A Transformer returns a new tokenizer that wraps src and optionally
 // transforms any tokens read from it.
 type Transformer func(src Tokenizer) Tokenizer
+
+// Map returns a Transformer that maps the tokens in the input using the given
+// mapping.
+func Map(mapping func(t xml.Token) xml.Token) Transformer {
+	return func(src Tokenizer) Tokenizer {
+		return mapper{
+			t: src,
+			f: mapping,
+		}
+	}
+}
+
+type mapper struct {
+	t Tokenizer
+	f func(t xml.Token) xml.Token
+}
+
+func (m mapper) Token() (xml.Token, error) {
+	tok, err := m.t.Token()
+	if err != nil {
+		return nil, err
+	}
+	return m.f(tok), nil
+}
+
+func (m mapper) Skip() error {
+	return m.t.Skip()
+}
+
+// Remove returns a Transformer that removes tokens for which f matches.
+func Remove(f func(t xml.Token) bool) Transformer {
+	return func(src Tokenizer) Tokenizer {
+		return remover{
+			t: src,
+			f: f,
+		}
+	}
+}
+
+type remover struct {
+	t Tokenizer
+	f func(t xml.Token) bool
+}
+
+func (r remover) Token() (t xml.Token, err error) {
+	for {
+		t, err = r.t.Token()
+		switch {
+		case err != nil:
+			return nil, err
+		case r.f(t):
+			continue
+		}
+		return
+	}
+	return
+}
+
+func (r remover) Skip() error {
+	return r.t.Skip()
+}
