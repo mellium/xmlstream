196d39d46eb2baa981ea4ed2e06b8efc658be4d8
Sam Whited <sam@samwhited.com>
2017-02-06 07:19:25 -0600

    Move package docs to new file

---
+++ doc.go
@@ -0,0 +1,7 @@
+// Copyright 2016 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+
+// Package xmlstream provides an API for streaming, transforming, and otherwise
+// manipulating XML data.
+package xmlstream // import "mellium.im/xmlstream"
--- transformer.go
+++ transformer.go
@@ -2,9 +2,7 @@
 // Use of this source code is governed by the BSD 2-clause license that can be
 // found in the LICENSE file.
 
-// Package xmlstream provides an API for streaming, transforming, and otherwise
-// manipulating XML data.
-package xmlstream // import "mellium.im/xmlstream"
+package xmlstream
 
 import (
 	"encoding/xml"
