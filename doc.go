4ae3ddf7eb0360c4453f1680b4177f4f5d30402d
Sam Whited <sam@samwhited.com>
2017-02-06 07:20:44 -0600

    Add unstable API warning

--- doc.go
+++ doc.go
@@ -4,4 +4,6 @@
 
 // Package xmlstream provides an API for streaming, transforming, and otherwise
 // manipulating XML data.
+//
+// BE ADVISED: The API is unstable and subject to change.
 package xmlstream // import "mellium.im/xmlstream"
