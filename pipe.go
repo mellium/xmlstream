382103d78b418df134d9b8352b726485ed407c90
Sam Whited <sam@samwhited.com>
2017-08-25 17:52:09 -0500

    Add XML equivalent of io.Pipe

---
+++ pipe.go
@@ -0,0 +1,197 @@
+// Copyright 2017 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+//
+// Code in this file copied from the Go io package:
+//
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE.GO file.
+
+package xmlstream
+
+import (
+	"encoding/xml"
+	"errors"
+	"io"
+	"sync"
+)
+
+// TODO: For now I've just copied this from io.pipe, but tokens are fundamentally
+//       different than byte slices. It would be nice to add some sort of
+//       buffering in the future so that multiple calls to write can happen
+//       before we have to call read. This would also give a purpose to the noop
+//       Flush() method. A buffer size of zero would behave exactly as this does
+//       now, so I think this can be done in a backwards compatible way (the
+//       only API change would be the addition of a BufferedPipe(n int) method).
+
+// A pipe is the shared pipe structure underlying PipeReader and PipeWriter.
+type pipe struct {
+	rl    sync.Mutex // gates readers one at a time
+	wl    sync.Mutex // gates writers one at a time
+	l     sync.Mutex // protects remaining fields
+	data  xml.Token  // data remaining in pending read/write
+	rwait sync.Cond  // waiting reader
+	wwait sync.Cond  // waiting writer
+	rerr  error      // if reader closed, error to give writes
+	werr  error      // if writer closed, error to give reads
+}
+
+func (p *pipe) Token() (t xml.Token, err error) {
+	// One reader at a time.
+	p.rl.Lock()
+	defer p.rl.Unlock()
+
+	p.l.Lock()
+	defer p.l.Unlock()
+	for {
+		if p.rerr != nil {
+			return nil, ErrClosedPipe
+		}
+		if p.data != nil {
+			break
+		}
+		if p.werr != nil {
+			return nil, p.werr
+		}
+		p.rwait.Wait()
+	}
+	t = p.data
+	p.data = nil
+	p.wwait.Signal()
+	return t, err
+}
+
+func (p *pipe) EncodeToken(t xml.Token) (err error) {
+	// One writer at a time.
+	p.wl.Lock()
+	defer p.wl.Unlock()
+	p.l.Lock()
+	defer p.l.Unlock()
+	if p.werr != nil {
+		err = ErrClosedPipe
+		return err
+	}
+	p.data = t
+	p.rwait.Signal()
+	for {
+		if p.data == nil {
+			break
+		}
+		if p.rerr != nil {
+			err = p.rerr
+			break
+		}
+		if p.werr != nil {
+			err = ErrClosedPipe
+			break
+		}
+		p.wwait.Wait()
+	}
+	p.data = nil // in case of rerr or werr
+	return err
+}
+
+func (p *pipe) rclose(err error) {
+	if err == nil {
+		err = ErrClosedPipe
+	}
+	p.l.Lock()
+	defer p.l.Unlock()
+	p.rerr = err
+	p.rwait.Signal()
+	p.wwait.Signal()
+}
+
+func (p *pipe) wclose(err error) {
+	if err == nil {
+		err = io.EOF
+	}
+	p.l.Lock()
+	defer p.l.Unlock()
+	p.werr = err
+	p.rwait.Signal()
+	p.wwait.Signal()
+}
+
+// ErrClosedPipe is the error used for read or write operations on a closed
+// pipe.
+var ErrClosedPipe = errors.New("xmlstream: read/write on closed pipe")
+
+// A PipeReader is the read half of a token pipe.
+type PipeReader struct {
+	p *pipe
+}
+
+// Token implements the xml.TokenReader interface.
+// It reads a token from the pipe, blocking until a writer arrives or the write
+// end is closed. If the write end is closed with an error, that error is
+// returned as err; otherwise err is io.EOF.
+func (r *PipeReader) Token() (t xml.Token, err error) {
+	return r.p.Token()
+}
+
+// Close closes the PipeReader; subsequent reads from the read half of the pipe
+// will return no bytes and EOF.
+func (r *PipeReader) Close() error {
+	r.CloseWithError(nil)
+	return nil
+}
+
+// CloseWithError closes the PipeReader; subsequent reads from the read half of
+// the pipe will return no tokens and the error err, or EOF if err is nil.
+func (r *PipeReader) CloseWithError(err error) {
+	r.p.rclose(err)
+}
+
+// A PipeWriter is the write half of a token pipe.
+type PipeWriter struct {
+	p *pipe
+}
+
+// EncodeToken implements the TokenWriter interface.
+// It writes a token to the pipe, blocking until one or more readers have
+// consumed all the data or the read end is closed.
+// If the read end is closed with an error, that err is returned as err;
+// otherwise err is ErrClosedPipe.
+func (w *PipeWriter) EncodeToken(t xml.Token) error {
+	return w.p.EncodeToken(t)
+}
+
+// Flush is currently a noop and always returns nil.
+func (w *PipeWriter) Flush() error {
+	return nil
+}
+
+// Close closes the PipeWriter; subsequent reads from the read half of the pipe
+// will return no bytes and EOF.
+func (w *PipeWriter) Close() error {
+	w.CloseWithError(nil)
+	return nil
+}
+
+// CloseWithError closes the PipeWriter; subsequent reads from the read half of
+// the pipe will return no tokens and the error err, or EOF if err is nil.
+func (w *PipeWriter) CloseWithError(err error) {
+	w.p.wclose(err)
+}
+
+// Pipe creates a synchronous in-memory pipe of tokens.
+// It can be used to connect code expecting an xml.TokenReader
+// with code expecting an xmlstream.TokenWriter.
+//
+// Reads and Writes on the pipe are matched one to one.
+// That is, each Write to the PipeWriter blocks until it has satisfied a Read
+// from the corresponding PipeReader.
+//
+// It is safe to call Read and Write in parallel with each other or with Close.
+// Parallel calls to Read and parallel calls to Write are also safe:
+// the individual calls will be gated sequentially.
+func Pipe() (*PipeReader, *PipeWriter) {
+	p := new(pipe)
+	p.rwait.L = &p.l
+	p.wwait.L = &p.l
+	r := &PipeReader{p}
+	w := &PipeWriter{p}
+	return r, w
+}
---
+++ pipe_test.go
@@ -0,0 +1,254 @@
+// Copyright 2017 Sam Whited.
+// Use of this source code is governed by the BSD 2-clause license that can be
+// found in the LICENSE file.
+//
+// Code in this file copied from the Go io package:
+//
+// Copyright 2009 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be found
+// in the LICENSE.GO file.
+
+package xmlstream_test
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"testing"
+	"time"
+
+	"mellium.im/xmlstream"
+)
+
+// TODO: This test package was a copy paste job with a mix of hand tweaking and
+//       sed to get tests to pass. It should probably be rewritten from scratch
+//       at some point.
+
+func checkWrite(t *testing.T, w xmlstream.TokenWriter, tok xml.Token) {
+	err := w.EncodeToken(tok)
+	if err != nil {
+		t.Errorf("write: %v", err)
+	}
+}
+
+// Test a single read/write pair.
+func TestPipe1(t *testing.T) {
+	r, w := xmlstream.Pipe()
+	go checkWrite(t, w, xml.CharData("hello, world"))
+	tok, err := r.Token()
+	if err != nil {
+		t.Errorf("read: %v", err)
+	} else if string(tok.(xml.CharData)) != "hello, world" {
+		t.Errorf("bad read: got %s", tok)
+	}
+	r.Close()
+	w.Close()
+}
+
+func reader(t *testing.T, r xml.TokenReader, c chan xml.Token) {
+	for {
+		tok, err := r.Token()
+		if err == io.EOF {
+			c <- nil
+			break
+		}
+		if err != nil {
+			t.Errorf("read: %v", err)
+		}
+		c <- tok
+	}
+}
+
+// Test a sequence of read/write pairs.
+func TestPipe2(t *testing.T) {
+	c := make(chan xml.Token)
+	r, w := xmlstream.Pipe()
+	go reader(t, r, c)
+	tok := xml.CharData("hello world")
+	for i := 0; i < 5; i++ {
+		err := w.EncodeToken(tok)
+		if err != nil {
+			t.Errorf("write: %v", err)
+		}
+		tt := <-c
+		if string(tt.(xml.CharData)) != string(tok) {
+			t.Errorf("wrote %v, read got %v", t, tt)
+		}
+	}
+	w.Close()
+	tt := <-c
+	if tt != nil {
+		t.Errorf("final read got %v", tt)
+	}
+}
+
+// TODO: Test a large write that requires multiple reads to satisfy.
+
+// Test read after/before writer close.
+
+type closer interface {
+	CloseWithError(error)
+	Close() error
+}
+
+type pipeTest struct {
+	async          bool
+	err            error
+	closeWithError bool
+}
+
+func (p pipeTest) String() string {
+	return fmt.Sprintf("async=%v err=%v closeWithError=%v", p.async, p.err, p.closeWithError)
+}
+
+var pipeTests = []pipeTest{
+	{true, nil, false},
+	{true, nil, true},
+	{true, io.ErrShortWrite, true},
+	{false, nil, false},
+	{false, nil, true},
+	{false, io.ErrShortWrite, true},
+}
+
+func delayClose(t *testing.T, cl closer, ch chan struct{}, tt pipeTest) {
+	time.Sleep(1 * time.Millisecond)
+	var err error
+	if tt.closeWithError {
+		cl.CloseWithError(tt.err)
+	} else {
+		err = cl.Close()
+	}
+	if err != nil {
+		t.Errorf("delayClose: %v", err)
+	}
+	ch <- struct{}{}
+}
+
+func TestPipeReadClose(t *testing.T) {
+	for _, tt := range pipeTests {
+		c := make(chan struct{}, 1)
+		r, w := xmlstream.Pipe()
+		if tt.async {
+			go delayClose(t, w, c, tt)
+		} else {
+			delayClose(t, w, c, tt)
+		}
+		tok, err := r.Token()
+		<-c
+		want := tt.err
+		if want == nil {
+			want = io.EOF
+		}
+		if err != want {
+			t.Errorf("read from closed pipe: %v want %v", err, want)
+		}
+		if tok != nil {
+			t.Errorf("read on closed pipe returned %v", tok)
+		}
+		if err = r.Close(); err != nil {
+			t.Errorf("r.Close: %v", err)
+		}
+	}
+}
+
+// Test close on Read side during Read.
+func TestPipeReadClose2(t *testing.T) {
+	c := make(chan struct{}, 1)
+	r, _ := xmlstream.Pipe()
+	go delayClose(t, r, c, pipeTest{})
+	tok, err := r.Token()
+	<-c
+	if tok != nil || err != xmlstream.ErrClosedPipe {
+		t.Errorf("read from closed pipe: %v, %v want %v, %v", tok, err, (xml.Token)(nil), xmlstream.ErrClosedPipe)
+	}
+}
+
+// Test write after/before reader close.
+
+func TestPipeWriteClose(t *testing.T) {
+	for _, tt := range pipeTests {
+		c := make(chan struct{}, 1)
+		r, w := xmlstream.Pipe()
+		if tt.async {
+			go delayClose(t, r, c, tt)
+		} else {
+			delayClose(t, r, c, tt)
+		}
+		err := w.EncodeToken(xml.CharData("hello, world"))
+		<-c
+		expect := tt.err
+		if expect == nil {
+			expect = xmlstream.ErrClosedPipe
+		}
+		if err != expect {
+			t.Errorf("write on closed pipe: %v want %v", err, expect)
+		}
+		if err = w.Close(); err != nil {
+			t.Errorf("w.Close: %v", err)
+		}
+	}
+}
+
+// Test close on Write side during Write.
+func TestPipeWriteClose2(t *testing.T) {
+	c := make(chan struct{}, 1)
+	_, w := xmlstream.Pipe()
+	go delayClose(t, w, c, pipeTest{})
+	err := w.EncodeToken(xml.CharData("hello, world"))
+	<-c
+	if err != xmlstream.ErrClosedPipe {
+		t.Errorf("write to closed pipe: %v want %v", err, xmlstream.ErrClosedPipe)
+	}
+}
+
+func TestWriteEmpty(t *testing.T) {
+	r, w := xmlstream.Pipe()
+	go func() {
+		w.EncodeToken(xml.CharData("hello, world"))
+		w.Close()
+	}()
+
+	r.Token()
+	r.Close()
+}
+
+func TestWriteNil(t *testing.T) {
+	r, w := xmlstream.Pipe()
+	go func() {
+		w.EncodeToken(nil)
+		w.Close()
+	}()
+	r.Token()
+	r.Close()
+}
+
+func TestWriteAfterWriterClose(t *testing.T) {
+	r, w := xmlstream.Pipe()
+
+	done := make(chan struct{})
+	var writeErr error
+	go func() {
+		err := w.EncodeToken(xml.CharData("hello"))
+		if err != nil {
+			t.Errorf("got error: %q; expected none", err)
+		}
+		w.Close()
+		writeErr = w.EncodeToken(xml.CharData("world"))
+		done <- struct{}{}
+	}()
+
+	var result string
+	tok, err := r.Token()
+	if err != nil {
+		t.Fatalf("err: %v", err)
+	}
+	result = string(tok.(xml.CharData))
+	<-done
+
+	if result != "hello" {
+		t.Errorf("got: %q; want: %q", result, "hello")
+	}
+	if writeErr != xmlstream.ErrClosedPipe {
+		t.Errorf("got: %q; want: %q", writeErr, xmlstream.ErrClosedPipe)
+	}
+}
