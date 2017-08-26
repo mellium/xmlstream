276c074c7b7132e730b793e3c157e2e95ea7bf23
Sam Whited <sam@samwhited.com>
2017-08-26 10:44:23 -0500

    docs: clarify that this requires a custom Go build
    See https://golang.org/cl/38791

--- doc.go
+++ doc.go
@@ -5,5 +5,8 @@
 // Package xmlstream provides an API for streaming, transforming, and otherwise
 // manipulating XML data.
 //
+// This package only builds against versions of Go that include this patch:
+// https://golang.org/cl/38791
+//
 // BE ADVISED: The API is unstable and subject to change.
 package xmlstream // import "mellium.im/xmlstream"
