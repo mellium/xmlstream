1e4e29185020ac8a76607d515e6a1a5ec971d444
Sam Whited <sam@samwhited.com>
2017-10-06 23:19:03 -0500

    Rename Copy parameters

--- copy.go
+++ copy.go
@@ -10,28 +10,28 @@ import (
 )
 
 // Copy consumes a TokenReader and writes its tokens to a TokenWriter unti
-// leither io.EOF is reached on d or an error occurs.
+// leither io.EOF is reached on src or an error occurs.
 // It returns the number of tokens copied and the first error encountered while
 // copying, if any.
 // If an error is returned by the reader or writer, copy returns it immediately.
 // Since Copy is defined as consuming the stream until the end, io.EOF is not
 // returned.
 // If no error would be returned, Copy flushes the TokenWriter when it is done.
-func Copy(e TokenWriter, d TokenReader) (n int, err error) {
+func Copy(dst TokenWriter, src TokenReader) (n int, err error) {
 	defer func() {
 		if err == nil || err == io.EOF {
-			err = e.Flush()
+			err = dst.Flush()
 		}
 	}()
 
 	var tok xml.Token
 	for {
-		tok, err = d.Token()
+		tok, err = src.Token()
 		if (err != nil && err != io.EOF) || (tok == nil && err == io.EOF) {
 			return n, err
 		}
 
-		if err := e.EncodeToken(tok); err != nil {
+		if err := dst.EncodeToken(tok); err != nil {
 			return n, err
 		}
 		n++
